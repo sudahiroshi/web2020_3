e17c79eb16d465b9558f5634d5099151a52a4b2f
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2020-07-01 03:01:38 +0900

    sql.jsの終了処理を修正

--- sql.js
+++ sql.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test.db');
 
-if( process.argv < 2 )	System.exit(0);
+if( process.argv.length < 3 )	process.exit(0);
 
 if( /select/.test( process.argv[2] )) {
 	db.each( process.argv[2], (err, result ) => {
